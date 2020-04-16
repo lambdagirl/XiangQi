c6dd4ed300cf8adce76742d8e2f40baab3cf172e
Peizhi Zheng <parisa1206@gmail.com>
2020-04-15 21:10:57 -0700

    init

---
+++ board.rb
@@ -0,0 +1,10 @@
+class Board
+    ROW = 10
+    COL = 8
+
+    def initialize
+        @grid = Array.new(ROW) { Array.new(COL, nil) }
+    end
+
+
+end
---
+++ main.rb
@@ -0,0 +1,8 @@
+require_relative './board.rb'
+
+def main
+    b = Board.new
+    require 'pry'; binding.pry
+end
+
+main
