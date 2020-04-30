af482aa0b3b8b60633b0f818c5f3e0ddcc0c139c
Peizhi Zheng <parisa1206@gmail.com>
2020-04-30 15:08:37 -0700

    add 蹩马腿

--- pieces/horse.rb
+++ pieces/horse.rb
@@ -1,8 +1,8 @@
 require_relative 'piece'
 require_relative 'stepable'
-#马
+#马 
 class Horse < Piece
-    include Stepable
+
 
     def name
         "H".colorize(color)
@@ -11,10 +11,56 @@ class Horse < Piece
     def deltas 
         [
             [1,2],
+            [1,-2],
+            [-1,2],
+            [-1,-2],
             [2,1],
             [2,-1],
-            [1,-2]
+            [-2,1],
+            [-2,-1]
         ]
     end
+
+    def blocks
+        
+        [[0,1],[0,-1],[1,0],[-1,0]]
+        
+    end
+
+    def moves
+        #蹩马腿
+
+        blocked = []
+        blocks.each_with_object([]) do |(dx, dy), moves|
+            cur_x, cur_y = pos
+            pos = [cur_x + dx, cur_y + dy]
+            next unless self.within_bound?(pos)
+            if !board.empty?(pos)
+                if dx == 0 and dy == 1
+                    deltas.each { |d| blocked << d if d[1]> 1 }
+                elsif dx == 0 and dy == -1
+                    deltas.each { |d| blocked << d if d[1]< -1  }
+                elsif dx == 1 and dy == 0
+                    deltas.each { |d| blocked << d if d[0]> 1   }
+                elsif dx == -1 and dy == 0
+                    deltas.each { |d| blocked << d if  d[0]< -1 }
+                end
+                p pos, blocked
+            end
+        end
+
+        deltas.each_with_object([]) do |(dx, dy), moves|
+          cur_x, cur_y = pos
+          pos = [cur_x + dx, cur_y + dy]
+    
+          next unless self.within_bound?(pos) && !blocked.include?([dx, dy])
+          if board.empty?(pos)
+            moves << pos
+          elsif board[pos].color != color
+            moves << pos
+          end
+        end
+      end
+
 
 end
